e54cf4575c7108598ad2d089a039262d4d5e1634
monzelb <monzelb@gmail.com>
2017-07-28 20:29:39 -0700

    started portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+
+});
+
+      function openNav() {
+    document.getElementById("mySidenav").classList.toggle("menu-toggle");
+}
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
