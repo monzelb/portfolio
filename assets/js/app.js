04fd932a6190ffae3e49d676b8443a0e8cbec02f
monzelb <monzelb@gmail.com>
2017-07-29 23:34:23 -0700

    day 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
 $( document ).ready(function() {
 
+  
 
 });
 
       function openNav() {
     document.getElementById("mySidenav").classList.toggle("menu-toggle");
 }
+
+$(window).on("load", function() {
+    $(".loader").fadeOut("slow");
+  })
 
 // function closeNav() {
 //     document.getElementById("mySidenav").style.width = "0";
