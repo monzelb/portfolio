0867be73f8779511fe00e400ee82e6869b167b3a
monzelb <monzelb@gmail.com>
2017-07-30 22:17:42 -0700

    got slidies to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,40 @@
 $( document ).ready(function() {
 
-  
+  $(".menu-items").mouseover(function(){
+    $(this).next(".slidies").animate({right: "90px"}, 25)
+  })
+  $(".menu-items").mouseleave(function(){
+    $(this).next(".slidies").animate({right: "0px"}, 3)
+  })
 
 });
 
-      function openNav() {
-    document.getElementById("mySidenav").classList.toggle("menu-toggle");
+function openNav() {
+  document.getElementById("mySidenav").classList.toggle("menu-toggle");
 }
+
+
+
+// $('#house').click(function () {
+//     if($("#home").is(':visible')){
+//     $('#home').fadeOut(function () {
+//         $('#categories').toggle('slide', {
+//             direction: 'left'
+//         }, 1000);
+//     });
+//     }
+//     else{
+//         $('#categories').toggle('slide', {
+//             direction: 'left'
+//         }, 1000, function(){ $('#cat_icon').fadeIn();});
+//     }
+// });
+
 
 $(window).on("load", function() {
     $(".loader").fadeOut("slow");
   })
+
 
 // function closeNav() {
 //     document.getElementById("mySidenav").style.width = "0";
